68075ba7b3a87bb47f5fc454398c955ff635e563
Sahil Pathan <sp776115@gmail.com>
2026-04-11 17:25:05 +0530

    Exercise 1.5: put everything in a single course object

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Part = (props) => {
-  // Ab props.part ek object hai, toh hume .name aur .exercises lagana padega
   return (
     <p>
       {props.part.name} {props.part.exercises}
@@ -12,12 +11,11 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
-  // Array ke elements ko access karne ke liye index [0], [1], [2] use karte hain
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </div>
   );
 };
@@ -26,37 +24,38 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-
-  // Saara data ek single array of objects mein daal diya
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  // Dekh bhai, saara data ab ek hi 'course' object ke andar hai
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
