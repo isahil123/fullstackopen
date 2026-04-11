9f1161a7c99e4bc1ed382902c143e2119d6c4788
Sahil Pathan <sp776115@gmail.com>
2026-04-11 17:50:33 +0530

    Exercise 1.8: unicafe step 3 - extracted Statistics component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 
+// Naya Component: Sirf Stats dikhane ke liye
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+  const average =
+    total === 0
+      ? 0
+      : (props.good * 1 + props.neutral * 0 + props.bad * -1) / total;
+  const positivePercentage = total === 0 ? 0 : (props.good / total) * 100;
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercentage} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  // Math calculations
-  const total = good + neutral + bad;
-  const average = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total;
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100;
 
   return (
     <div>
@@ -17,14 +34,8 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      {/* Nayi cheezein */}
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positivePercentage} %</p>
+      {/* Yahan humne apne naye component ko call kiya aur data pass kar diya */}
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
