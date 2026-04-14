664537f73742cc98c09bdb5f188a8cc13f520b2c
Sahil Pathan <sp776115@gmail.com>
2026-04-14 12:03:14 +0530

    Exercise 2.4-2.5: handled multiple courses and refactored into modules

---
+++ part2/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((s, p) => s + p.exercises, 0);
+  return <strong>total of {sum} exercises</strong>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+export default Course;
