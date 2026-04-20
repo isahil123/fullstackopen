3b57a2f86701ae00f41ae4fa9cece1c85ade4a48
Sahil Pathan <sp776115@gmail.com>
2026-04-20 15:58:06 +0530

    new

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -1,27 +1,28 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Saare /api/blogs wale requests seedha blogsRouter ke paas jayenge
-app.use('/api/blogs', blogsRouter)
+app.use("/api/blogs", blogsRouter);
 
-module.exports = app
+module.exports = app;
