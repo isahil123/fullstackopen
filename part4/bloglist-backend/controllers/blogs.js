f996153f7906f6b94249f51979fbb61fa9fc2289
Sahil Pathan <sp776115@gmail.com>
2026-04-21 18:38:09 +0530

    now will ecrypt

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -24,5 +24,27 @@ blogsRouter.post("/", async (request, response) => {
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog);
 });
+// Ex 4.13: Blog delete karne ka code
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end(); // 204 matlab 'No Content' (Successfully deleted)
+});
 
+// Ex 4.14: Blog update (likes badhane) ka code
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  // { new: true } lagana zaroori hai taaki Mongoose update hone ke baad naya data bheje
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
 module.exports = blogsRouter;
